6ab99500cde2b9b6726f8a4d1497ded3a7d90357
Liam Frager <liamfrager@yahoo.com>
2024-10-16 19:00:03 +0000

    implement dao functions in services

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,12 +1,53 @@
 package Service;
 
+import DAO.AccountDAO;
 import Model.Account;
 
 public class AccountService {
-    public Account Register(Account account) {
-        return account;
+    public AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
     }
-    public Account Login(Account account) {
-        return account;
+
+    /**
+     * Register a user with a given account if the account does not exist.
+     * @param account The account of the user to register.
+     * @return The account of the registered user.
+     */
+    public Account Register(Account account) throws Exception {
+        // If the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist.
+        if (account.username.length() > 0 && account.password.length() >= 4 && userExists(account.username))
+            return accountDAO.createAccount(account);
+        throw new Exception();
+    }
+
+    /**
+     * Login a user with a given account.
+     * @param account The account of the user to login.
+     * @return The account of the logged in user.
+     */
+    public Account Login(Account account) throws Exception {
+        // If the username and password provided in the request body JSON match a real account existing on the database.
+        Account validAccount = accountDAO.getAccountByUsernameAndPassword(account.username, account.password);
+        if (validAccount != null)
+            return account;
+        throw new Exception();
+    }
+
+    /**
+     * @param id The ID of the user to check.
+     * @return A boolean indicating whether a user exists with the given ID.
+     */
+    public boolean userExists(int id) {
+        return accountDAO.getAccountByID(id) != null;
+    }
+
+    /**
+     * @param username The username of the user to check.
+     * @return A boolean indicating whether a user exists with the given username.
+     */
+    public boolean userExists(String username) {
+        return accountDAO.getAccountByUsername(username) != null;
     }
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -2,27 +2,88 @@ package Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import DAO.MessageDAO;
 import Model.Message;
 
 public class MessageService {
+    public MessageDAO messageDAO;
+    public AccountService accountService;
 
+    public MessageService(){
+        messageDAO = new MessageDAO();
+        accountService = new AccountService();
+    }
+
+    /**
+     * Get all messages.
+     * @return A list of all messages.
+     */
     public List<Message> getAllMessages() {
-        return new ArrayList<Message>();
-    }
-    public Message getMessageByID(int id) {
-        return new Message();
-    }
-    public Message postMessage(Message message) {
-        return message;
-    }
-    public boolean patchMessageByID(int id, Message message) {
-        return true;
-    }
-    public Message deleteMessageByID(int id) {
-        return new Message();
-    }
-    public List<Message> getAllMessagesByAccountID(int id) {
-        return new ArrayList<Message>();
+        return messageDAO.getAllMessages();
     }
     
+    /**
+     * Get a message with a given ID.
+     * @param id The ID of the message to be returned.
+     * @return The message with the given ID.
+     */
+    public Message getMessageByID(int id) {
+        return messageDAO.getMessageByID(id);
+    }
+    
+    /**
+     * Add a new message to the database if the message is valid.
+     * @param message The message to be added.
+     * @return The new message.
+     * @throws Exception The message is invalid.
+     */
+    public Message postMessage(Message message) throws Exception {
+        // If the message_text is not blank, is under 255 characters, and posted_by refers to a real, existing user.
+        if (message.message_text.length() > 0 && message.message_text.length() < 255 && accountService.userExists(message.posted_by))
+            return messageDAO.insertMessage(message);
+        throw new Exception();
+    }
+    
+    /**
+     * Update a message with a given ID if the given new_message is valid.
+     * @param id The ID of the message to update.
+     * @param new_message The new message that will replace the old message.
+     * @return The updated message.
+     * @throws Exception The new message is invalid.
+     */
+    public Message patchMessageByID(int id, Message new_message) throws Exception {
+        // If the message id already exists and the new message_text is not blank and is not over 255 characters
+        if (messageExists(id) && new_message.message_text.length() > 0 && new_message.message_text.length() < 255)
+            return messageDAO.updateMessageByID(id, new_message);
+        throw new Exception();
+    }
+    
+    /**
+     * Delete a message with a given ID if it exists.
+     * @param id The ID of the message to delete.
+     * @return The deleted message, or null if the message didn't exist.
+     */
+    public Message deleteMessageByID(int id) {
+        if (messageExists(id))
+            return messageDAO.deleteMessageByID(id);
+        return null;
+    }
+    
+    /**
+     * Get all messages posted by a user with the given ID.
+     * @param id The ID of the user whose messages will be returned. Returns null if the ID is invalid
+     * @return A list of all messages by the user with the given ID.
+     */
+    public List<Message> getAllMessagesByAccountID(int id) {
+        return messageDAO.getAllMessagesByAccountID(id);
+    }
+
+    
+    /**
+     * @param id The ID of the message to check.
+     * @return A boolean indicating whether a message exists with the given ID.
+     */
+    public boolean messageExists(int id) {
+        return messageDAO.getMessageByID(id) != null;
+    }
 }
